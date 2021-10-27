c9ba0b18e9d7c2f6068a0ecbe463124f3c4e2b91
Eric1833 <qq649486419@gmail.com>
2021-10-26 19:58:41 -0500

    Add update button on main page to get the weekly summary
    need to store the date to local then its will return the value for cardio or strength.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
-
+let strengthWork;
+let reps;
+let weight;
+let strengthSummary;
+let cardioWork;
+let timeWork;
+let distance;
+let cardioSummary;
 function trackerAlert() {
   alert("Don't forget to log your mood and soreness!")
 }
@@ -8,12 +15,13 @@ function workoutType(choice) {
     if (choice == 1) {
         document.getElementById("cardio").disabled = true;
         document.getElementById("strength").disabled = false;
-        let cardioWork = window.prompt("Enter name of workout");
-        let timeWork = window.prompt("Enter time for timer");
-        let distance = window.prompt("Enter distance");
-        // I don't know why we need to input that. And how we use for that.
+        cardioWork = window.prompt("Enter name of workout");
+        timeWork = window.prompt("Enter time for timer");
+        distance = window.prompt("Enter distance");
+        cardioSummary = "cardioWork: " + cardioWork + " timeWork :" + timeWork + " distance : " + distance;
+        // I don't know why we need to input that. And how we use for that. we can just set up the name and time and the distance for the selectWorkout;
         if (document.getElementById("cardio").disabled == true) {
-            alert("cardioWork: " + cardioWork + " timeWork :" + timeWork + " distance : " + distance);
+            alert(cardioSummary);
             window.location.href = "../HTML/timer.html";
         }
 
@@ -21,17 +29,25 @@ function workoutType(choice) {
     if (choice == 2) {
         document.getElementById("strength").disabled = true;
         document.getElementById("cardio").disabled = false;
-        let strengthWork = window.prompt("Enter name of workout");
-        let reps = window.prompt("Enter time for timer");
-        let weight = window.prompt("Enter distance");
+        strengthWork = window.prompt("Enter name of workout");
+        reps = window.prompt("Enter time for timer");
+        weight = window.prompt("Enter distance");
+        strengthSummary = "strengthWork: " + strengthWork + " reps :" + reps + " weight : " + weight;
         if (document.getElementById("strength").disabled == true)
         {
-            alert("strengthWork: " + strengthWork + " reps :" + reps + " weight : " + weight);
+            alert(strengthSummary);
             window.location.href = "../HTML/timer.html";
         }
     }
 }
 
+function weeklySummary()
+{
+    cardioSummary = "cardioWork: " + cardioWork + " timeWork :" + timeWork + " distance : " + distance + "\n";
+    strengthSummary = "strengthWork: " + strengthWork + " reps :" + reps + " weight : " + weight + "\n";
+    var textarea = document.getElementById("weeklySummary");
+    textarea.innerHTML = "Cardio summary: " + cardioSummary + "Strength summary: " + strengthSummary;
+}
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
 output.innerHTML = slider.value;
